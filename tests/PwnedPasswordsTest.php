ba5965d2ea26656bf5dfd3f215c7995d8562f275
nickurt <nickurt@users.noreply.github.com>
2018-02-27 13:22:29 +0100

    added some more tests ..

--- tests/PwnedPasswordsTest.php
+++ tests/PwnedPasswordsTest.php
@@ -3,6 +3,7 @@
 namespace nickurt\PwnedPasswords\Tests;
 
 use Orchestra\Testbench\TestCase;
+use PwnedPasswords;
 
 class PwnedPasswordsTest extends TestCase
 {
@@ -38,13 +39,29 @@ class PwnedPasswordsTest extends TestCase
     }
 
     /** @test */
+    public function test_it_can_work_with_container()
+    {
+        $this->assertInstanceOf(\nickurt\PwnedPasswords\PwnedPasswords::class, $this->app['PwnedPasswords']);
+    }
+
+    /** @test */
+    public function test_it_can_work_with_facade()
+    {
+        $this->assertSame('nickurt\PwnedPasswords\Facade', (new \ReflectionClass(PwnedPasswords::class))->getName());
+
+        $this->assertSame('https://api.pwnedpasswords.com', PwnedPasswords::getApiUrl());
+        $this->assertNull(PwnedPasswords::getPassword());
+        $this->assertSame(10, PwnedPasswords::getFrequency());
+    }
+
+    /** @test */
     public function test_it_will_work_with_validation_rule()
     {
         $val1 = $this->validate('administrator', 1000);
 
         $this->assertFalse($val1->passes());
         $this->assertSame(1, count($val1->messages()->get('password')));
-        $this->assertSame('pwned-passwords::pwned-passwords.this_is_a_pwned_password', $val1->messages()->first('password'));
+        $this->assertSame('This is a pwned password', $val1->messages()->first('password'));
 
         $val2 = $this->validate('administrator', 17600);
 
@@ -90,7 +107,8 @@ class PwnedPasswordsTest extends TestCase
     protected function getPackageAliases($app)
     {
         return [
-            'Cache' => \Illuminate\Support\Facades\Cache::class
+            'Cache' => \Illuminate\Support\Facades\Cache::class,
+            'PwnedPasswords' => \nickurt\PwnedPasswords\Facade::class
         ];
     }
 
@@ -101,7 +119,8 @@ class PwnedPasswordsTest extends TestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Illuminate\Cache\CacheServiceProvider::class
+            \Illuminate\Cache\CacheServiceProvider::class,
+            \nickurt\PwnedPasswords\ServiceProvider::class
         ];
     }
 }
